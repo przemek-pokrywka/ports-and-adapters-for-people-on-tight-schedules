4ada7aed3301b50675ffd852d2c41f64cf9d0e29
Przemysław Pokrywka <przemyslaw.pokrywka@siriusxm.com>
2026-06-13 18:56:15 +0200

    back to dist

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 export default {
   base: "/ports-and-adapters-for-people-on-tight-schedules/",
   build: {
-    outDir: "ports-and-adapters-for-people-on-tight-schedules",
     minify: false
   }
 }
